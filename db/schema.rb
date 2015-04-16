a8f21835e30835290dedb6fa871eb49f5fd81996
rhoen <rhoen.pa@gmail.com>
2015-04-16 11:09:50 -0400

    require session_token at db level

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150416132813) do
+ActiveRecord::Schema.define(version: 20150416150548) do
 
   create_table "users", force: true do |t|
     t.string   "email",           null: false
     t.string   "password_digest", null: false
-    t.string   "session_token"
+    t.string   "session_token",   null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
