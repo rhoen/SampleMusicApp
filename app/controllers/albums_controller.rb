50e14d29fc0c80a5198a18ab6372b2e5c2ba92e3
rhoen <rhoen.pa@gmail.com>
2015-04-16 18:05:59 -0400

    update links on pages, repurpose album page

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -7,10 +7,23 @@ class AlbumsController < ApplicationController
   end
 
   def create
-
+    @album = Album.new(album_params)
+    if @album.save
+      redirect_to album_url(@album)
+    else
+      fail
+      flash.now[:errors] = @album.errors.full_messages
+      render :new
+    end
   end
 
   def show
+    @album = Album.find(params[:id])
+    render :show
+  end
 
+  private
+  def album_params
+    params.require(:album).permit(:name, :band_id)
   end
 end
